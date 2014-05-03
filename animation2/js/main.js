7145bc04ba6daffa2b86aef7aaa70c4d506dfa62
Stefan Laimer <slaimer.mmt-b2012@fh-salzburg.ac.at>
2014-05-03 14:24:08 +0200

    modified the animations

--- animation2/js/main.js
+++ animation2/js/main.js
@@ -1,12 +1,11 @@
 
 $(function(){
-    var animFunc = new MotionPathEffect($('#animpath')[0].pathSegList);
-    var elem = $('#animatedElem, #webanimation .animatedElem')[0];
+    var path = document.getElementById("animpath").cloneNode();
+    var animFunc = new MotionPathEffect(path.pathSegList);
+    var elem = document.getElementById('animatedElem');
 
     var animation = new Animation(elem, animFunc,
-        {
-            duration: 4, iterations: Infinity
-        }
+        { duration: 4, iterations: Infinity }
     );
 
     var player = document.timeline.play(animation);
--- animation3/js/main.js
+++ animation3/js/main.js
@@ -1,23 +1,24 @@
 
-$(function(){
+var c=document.getElementById("myCanvas");
+var ctx=c.getContext("2d");
 
-    var c=document.getElementById("myCanvas");
-    var ctx=c.getContext("2d");
+var getCoordinates = function(val){
+    var r = val * c.width / 1.5;
+    var phi = (val * 45) % 360;
+    return {
+        x: r * Math.cos(phi) + c.width/2,
+        y: r * Math.sin(phi) + c.height/2
+    };
+};
 
-    var animation = new Animation(null,
-        function(val, anim, oldval){
-            if(oldval>val){
-                ctx.clearRect(0,0,150,150);
-                ctx.beginPath();
-            }
-            ctx.moveTo(oldval*150,oldval*150);
-            ctx.lineTo(val*150,val*150);
-            ctx.stroke();
-        },
-        {
-            duration: 4
-        }
-    );
+var animation = new Animation(null,
+    function(val, anim, oldval){
+        oldval = getCoordinates(oldval);
+        val = getCoordinates(val);
+        ctx.moveTo(oldval.x, oldval.y);
+        ctx.lineTo(val.x, val.y);
+        ctx.stroke();
+    },
+4);
 
-    var player = document.timeline.play(animation);
-});
+var player = document.timeline.play(animation);
